a0c1a9b7571efcc2da4b1d7dd366c27ef2a79027
DR <david@va7eex.ca>
2020-04-20 22:23:51 -0700

    Errant CRLF?

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
-cp /usr/share/zoneinfo/$TZ /etc/localtime
+#!/bin/sh
+cp /usr/share/zoneinfo/$TZ /etc/localtie
 echo $TZ > /etc/timezone
 
 rtl_433 -M newmodel -F mqtt://$MQTT_IP:$MQTT_PORT,retain=0,devices=sensors/rtl_433/[model:""]/[id:0] -C $UNITS
