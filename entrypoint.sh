d0235e4b915381b289d496f36734980edad8bf78
DR <david@va7eex.ca>
2020-06-07 13:54:56 -0700

    Keyvalue error

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 cp /usr/share/zoneinfo/$TZ /etc/localtie
 echo $TZ > /etc/timezone
 
-rtl_433 -d $INDEX -M newmodel -F mqtt://$MQTT_IP:$MQTT_PORT,retain=0,devices=sensors/rtl_433/[model:""]/[id:0] -C $UNITS
+rtl_433 -d $DEVICEINDEX -M newmodel -F mqtt://$MQTT_IP:$MQTT_PORT,retain=0,devices=sensors/rtl_433/[model:""]/[id:0] -C $UNITS
